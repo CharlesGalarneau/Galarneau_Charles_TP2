d55eecb6e7e78ea646783828e3f4b4303e102628
CharlesGalarneau <94868285+CharlesGalarneau@users.noreply.github.com>
2022-03-25 10:52:57 -0400

    Commit Personnage
    Modification au animation des personnages, et commencement du script de pathing.

--- Assets/Script/Pathing Base.cs
+++ Assets/Script/Pathing Base.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PathingBase : MonoBehaviour
 {
+    private NavMeshAgent Personnages;
+    public Vector3 Destionation;
+
     // Start is called before the first frame update
     void Start()
     {
+        Destionation = Destination();
+        Personnages = GetComponent<NavMeshAgent>();
+        Personnages.SetDestination(Destionation);
         
     }
-
-    // Update is called once per frame
-    void Update()
+    Vector3 Destination()
     {
+
+        float limiteX_left = 0f;
+        float limiteX_Right = 0f;
+        float limiteZ = 0f;
+        return new Vector3(limiteX_left, limiteX_Right, limiteZ);
+        // Update is called once per frame
+        void Update()
+        {
+            if 
+        }
         
     }
 }
